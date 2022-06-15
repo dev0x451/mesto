1052fb7b6ef3e3567153c3cfefbee59e1721a4ee
Arkady Schapov <sigma696@gmail.com>
2022-06-15 23:04:50 +0300

    замечания после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
 const editButton = document.querySelector('.profile__edit-button');
-const closeIcon = document.querySelector('.popup__close-icon');
+const closeIcon = document.querySelector('.popup__close-button');
 const inputName = document.querySelector('.popup__input_name');
 const inputJob = document.querySelector('.popup__input_job');
 const profileName = document.querySelector('.profile__name');
@@ -12,20 +12,20 @@ closeIcon.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 function showPopup() {
-    inputName.value = profileName.textContent;
-    inputJob.value = profileJob.textContent;
-    popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-    inputName.value = profileName.textContent;
-    inputJob.value = profileJob.textContent;
-    popup.classList.remove('popup_opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value;
-    popup.classList.remove('popup_opened');
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  popup.classList.remove('popup_opened');
 }
